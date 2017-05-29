cce70df97e7cbfb928738241ef00d55221028d15
Manasses Ferreira <manasses.ferreira@gmail.com>
2017-05-29 17:58:36 -0300

    Update exe5.c

--- ap2/exe5.c
+++ ap2/exe5.c
@@ -1,6 +1,24 @@
-#include <stdio.h> //FILE, fgets, printf
-#include <string.h> //strtok
-#include <stdlib.h> //atof
+/*
+*Learning codes for C language
+*Copyright (C) 2017  Manassés Ferreira Neto
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h> //FILE, printf
+#include <string.h> //strtok, fgets
+#include <stdlib.h> //atof, atoi
 #define alunos 50
 #define MaiorNome 137
 
@@ -15,40 +33,28 @@ void iniciaTurma(tur *uma_turma){
   char const* const fileName = "./engComp.csv";
   FILE* file = fopen(fileName, "r"); 
   char line[256];
-
-  int aluno=0;
-  unsigned int uma_matricula;
-  float uma_mediaFinal;
-
-  while (fgets(line, sizeof(line), file) && aluno < alunos) {
-    //printf("%s", line); 
+  int aluno=0;  
+  while ( fgets(line, sizeof(line), file) && aluno < alunos ) {
     char *p;
-
+    
     p = strtok(line, ",");
     if(p){
       strncpy(uma_turma[aluno].nome, p, sizeof(p)*2);
-      //printf("%s\n", engComp[aluno].nome);
     }
 
     p = strtok(NULL, ",");
     if(p){
-      uma_matricula=atoi(p);
-      //printf("%d\n", uma_matricula);
-      uma_turma[aluno].matricula=uma_matricula;
+      uma_turma[aluno].matricula=atoi(p);
     }
 
     p = strtok(NULL, ",");
     if(p){
-      uma_mediaFinal=atof(strtok(p,"\n"));
-      //printf("%f", uma_mediaFinal);
-      uma_turma[aluno].mediaFinal=uma_mediaFinal;
+      uma_turma[aluno].mediaFinal=atof(strtok(p,"\n"));
     }
-    //printf("\n\n");
+
     aluno++;
   }
-
   fclose(file);
-
 }
 
 void imprimeTurma(tur *T){
